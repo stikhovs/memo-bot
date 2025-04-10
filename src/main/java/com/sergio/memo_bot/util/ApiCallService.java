a9f80e3002b83257b1100ef71c616be5551ae35e
Sergei Tikhov <s.tikhov@unabank.com>
2025-04-11 01:06:50 +0300

    Impl security config

--- src/main/java/com/sergio/memo_bot/util/ApiCallService.java
+++ src/main/java/com/sergio/memo_bot/util/ApiCallService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -25,6 +24,7 @@ import static com.sergio.memo_bot.util.UrlConstant.*;
 public class ApiCallService {
 
     private final RestTemplate restTemplate;
+    private final RequestBuilder requestBuilder;
 
     public UserDto saveUser(UserDto userDto) {
         log.info("Registering user: {}", userDto);
@@ -131,9 +131,7 @@ public class ApiCallService {
     private <T> ResponseEntity<T> get(String url, Class<T> resultType) {
         try {
             return restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    RequestEntity.EMPTY,
+                    requestBuilder.build(HttpMethod.GET, restTemplate.getUriTemplateHandler().expand(url)),
                     ParameterizedTypeReference.forType(TypeToken.get(resultType).getType())
             );
         } catch (HttpClientErrorException e) {
@@ -147,9 +145,7 @@ public class ApiCallService {
     private <T> ResponseEntity<List<T>> getList(String url, Class<T> elementType) {
         try {
             return restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    RequestEntity.EMPTY,
+                    requestBuilder.build(HttpMethod.GET, restTemplate.getUriTemplateHandler().expand(url)),
                     ParameterizedTypeReference.forType(TypeToken.getParameterized(List.class, elementType).getType())
             );
         } catch (HttpClientErrorException e) {
@@ -161,9 +157,8 @@ public class ApiCallService {
     }
 
     private <T, R> ResponseEntity<R> post(String url, T requestBody, Class<R> responseType) {
-        return restTemplate.postForEntity(
-                url,
-                requestBody,
+        return restTemplate.exchange(
+                requestBuilder.build(HttpMethod.POST, restTemplate.getUriTemplateHandler().expand(url), requestBody),
                 responseType
         );
     }
@@ -171,7 +166,7 @@ public class ApiCallService {
     private <T, R> ResponseEntity<R> put(String url, T requestBody, Class<R> responseType) {
         try {
             return restTemplate.exchange(
-                    RequestEntity.put(restTemplate.getUriTemplateHandler().expand(url)).body(requestBody),
+                    requestBuilder.build(HttpMethod.PUT, restTemplate.getUriTemplateHandler().expand(url), requestBody),
                     responseType
             );
         } catch (HttpClientErrorException e) {
@@ -183,6 +178,7 @@ public class ApiCallService {
     }
 
     private  void delete(String url) {
-        restTemplate.delete(restTemplate.getUriTemplateHandler().expand(url));
+        requestBuilder.build(HttpMethod.DELETE, restTemplate.getUriTemplateHandler().expand(url));
     }
+
 }
