962774501f89c9ac877960f3aa82c5f91cdf9519
Sergei Tikhov <s.tikhov@unabank.com>
2025-04-24 21:57:03 +0300

    Validate card set import

--- src/main/java/com/sergio/memo_bot/command_handler/card_set/import_card_set/ImportCardsResponse.java
+++ src/main/java/com/sergio/memo_bot/command_handler/card_set/import_card_set/ImportCardsResponse.java
@@ -7,14 +7,20 @@ import com.sergio.memo_bot.dto.ProcessableMessage;
 import com.sergio.memo_bot.persistence.entity.ChatTempData;
 import com.sergio.memo_bot.persistence.service.ChatAwaitsInputService;
 import com.sergio.memo_bot.persistence.service.ChatTempDataService;
-import com.sergio.memo_bot.state.CommandType;
+import com.sergio.memo_bot.reply.BotMessageReply;
 import com.sergio.memo_bot.reply.BotPartReply;
+import com.sergio.memo_bot.reply.NextReply;
 import com.sergio.memo_bot.reply.Reply;
+import com.sergio.memo_bot.state.CommandType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 
 @Slf4j
 @Component
@@ -36,26 +42,58 @@ public class ImportCardsResponse implements CommandHandler {
         chatAwaitsInputService.clear(chatId);
 
         String data = processableMessage.getText();
-        System.out.println(data);
-        List<CardDto> cards = data.trim().lines()
-                .map(card -> {
-                    String[] split = card.trim().split("  ");
-                    return CardDto.builder().frontSide(split[0]).backSide(split[1]).build();
-                })
-                .toList();
 
-        chatTempDataService.clearAndSave(chatId,
-                ChatTempData.builder()
-                        .chatId(chatId)
-                        .data(new Gson().toJson(cards))
-                        .command(CommandType.IMPORT_CARDS_RESPONSE)
-                        .build()
-        );
+        if (isValid(data)) {
+            List<CardDto> cards = data.trim().lines()
+                    .filter(this::validLine)
+                    .map(card -> {
+                        String[] split = card.trim().split("  ");
+                        return CardDto.builder()
+                                .frontSide(split[0].trim())
+                                .backSide(split[1].trim())
+                                .build();
+                    })
+                    .toList();
 
-        return BotPartReply.builder()
-                .chatId(chatId)
-                .previousProcessableMessage(processableMessage)
-                .nextCommand(CommandType.IMPORT_CARD_SET_RESPONSE)
+            if (isEmpty(cards)) {
+                return getErrorMessage(processableMessage);
+            }
+
+            chatTempDataService.clearAndSave(chatId,
+                    ChatTempData.builder()
+                            .chatId(chatId)
+                            .data(new Gson().toJson(cards))
+                            .command(CommandType.IMPORT_CARDS_RESPONSE)
+                            .build()
+            );
+
+            return BotPartReply.builder()
+                    .chatId(chatId)
+                    .previousProcessableMessage(processableMessage)
+                    .nextCommand(CommandType.IMPORT_CARD_SET_RESPONSE)
+                    .build();
+        } else {
+            return getErrorMessage(processableMessage);
+        }
+    }
+
+    private boolean validLine(String card) {
+        String[] split = card.trim().split("  ");
+        return ArrayUtils.getLength(split) == 2;
+    }
+
+    private boolean isValid(String data) {
+        return StringUtils.isNotBlank(data);
+    }
+
+    private Reply getErrorMessage(ProcessableMessage processableMessage) {
+        return BotMessageReply.builder()
+                .chatId(processableMessage.getChatId())
+                .text("Пожалуйста, попробуйте снова")
+                .nextReply(NextReply.builder()
+                        .previousProcessableMessage(processableMessage)
+                        .nextCommand(CommandType.IMPORT_CARDS_REQUEST)
+                        .build())
                 .build();
     }
 }
